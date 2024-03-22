c06a3dfaf0abf134eb8f116ef54fc074b9c8f8ba
ishaswamy <144390873+ishaswamy@users.noreply.github.com>
2024-03-22 12:01:53 -0400

    got rid of backend file since its more organized now and fixed the database connection in MyDatabasePal

--- MyDatabasePal.py
+++ MyDatabasePal.py
@@ -1,12 +1,14 @@
 """
-Imports the database for MangoDB.
+Imports the database for MongoDB.
 
 """
+import MyClassPal;
 
 from pymongo import MongoClient
 
 # Connect to MongoDB
-client = MongoClient("mongodb+srv://cluster0.ydug9w5.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=Cluster0")
+#client = MongoClient("mongodb+srv://cluster0.ydug9w5.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient('mongodb://localhost:27017/')
 
 # Access a database
 db = client['MyCalPal']
@@ -15,7 +17,7 @@ db = client['MyCalPal']
 collection = db['item']
 
 # Insert a document
-result = collection.insert_one({'name': 'Cookie', 'calories': 340})
+result = collection.insert_one({'name': 'Orange', 'calories': 90})
 print(result)
 
 # Close the connection
